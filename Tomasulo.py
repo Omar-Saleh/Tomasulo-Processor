d3b6813b3f2c4e51ca48e438ed36f474efcd27c0
Omar Saleh <omarsaleh0106@gmail.com>
2015-12-04 01:28:28 +0200

    branching now works properly

--- Tomasulo.py
+++ Tomasulo.py
@@ -14,7 +14,7 @@ class Tomasulo(object):
 		self.instructions = self.m.instructions
 		self.registerStatus = RegisterStatus()
 		ROBSize = int(input("Please enter ROB size: "))
-		self.rob = ROB(self.ROBSize)
+		self.rob = ROB(ROBSize)
 		self.pipelineWidth = int(input("Please enter pipeline width: "))
 		bufferSize = int(input("Please enter size of instruction buffer: "))	
 		AddUnitSize = int(input("Please enter number of ADD Units: "))
@@ -36,30 +36,39 @@ class Tomasulo(object):
 			self.reservationStations.append(ReservationStation("LDST",LDSTUnitCycle))
 
 		for i in range(LogicUnitSize):
-			self.reservationStations.append(ReservationStation("Logic", LogicUnitCycle))
-
-
-		for i in range(self.LDSTUnitSize):
-			self.reservationStations.append(ReservationStation("LOGIC",self.LDSTUnitCycle))
+			self.reservationStations.append(ReservationStation("LOGIC", LogicUnitCycle))
 
 
 		self.currentPC = self.m.pc
-		self.instructionBuffer = InstructionBuffer(self.bufferSize)
+		self.instructionBuffer = InstructionBuffer(bufferSize)
+		self.branchOverride = None
+		self.toggleBranch = False
 
 	def fetch(self):
 		for i in range(self.pipelineWidth):
+			if self.branchOverride != None:
+				# print("here")
+				self.currentPC = int(self.branchOverride)
+				self.branchOverride = None
 			if not self.instructionBuffer.isFull() and self.currentPC in self.instructions.keys():
-				self.instructionBuffer.insert(self.instructions[self.currentPC])
 				if self.instructions[self.currentPC][0].lower() in self.m.parser.branchingInstructions:
 					if self.instructions[self.currentPC][0] == "jmp" and self.registerStatus.registers[self.instructions[self.currentPC][1]] == None:
 						self.instructionBuffer.insert(self.instructions[self.currentPC])
 						self.currentPC += 2 + self.registerFile[self.instructions[self.currentPC][1]] + int(self.instructions[self.currentPC][2])
 					elif self.instructions[self.currentPC][0] == "beq":
-						if self.instructions[self.currentPC][3] & (1 << 6) != 0:
-							pass
+						if int(self.instructions[self.currentPC][3]) < 0:
+							self.instructions[self.currentPC].append(self.currentPC + 2)
+							self.instructionBuffer.insert(self.instructions[self.currentPC])
+							self.currentPC += 2 + int(self.instructions[self.currentPC][3])
 						else:
-							pass
+							print(self.instructions[self.currentPC])
+							self.instructions[self.currentPC].append(self.currentPC + 2)
+							self.instructionBuffer.insert(self.instructions[self.currentPC])
+							self.currentPC += 2
+					else:
+						break
 				else:
+					self.instructionBuffer.insert(self.instructions[self.currentPC])
 					self.currentPC += 2
 
 	def issue(self):
@@ -68,7 +77,7 @@ class Tomasulo(object):
 		while self.instructionBuffer.peek() != None and not self.rob.isFull():
 			# print("HI")
 			instruction = self.instructionBuffer.peek()
-			if instruction[0] in self.m.parser.addInstructions :    #check if its in the add unit
+			if instruction[0] in self.m.parser.addInstructions or instruction[0] in self.m.parser.branchingInstructions:    #check if its in the add unit
 				if not self.helper(instruction, "ADD"):
 					break
 			elif instruction[0] in self.m.parser.mulInstructions :
@@ -88,39 +97,59 @@ class Tomasulo(object):
 
 	def helper(self,instruction, s):
 		# print("here")
+		# print(instruction)
 		reservedPlace = False
 		for i in range(len(self.reservationStations)):
+			print(self.reservationStations[i].busy)
 			#print(not self.reservationStations[i].check(), self.reservationStations[i].name, s)
 			if self.reservationStations[i].name == s and not self.reservationStations[i].check():
 				# print("here")
 				# print(instruction)
 				reservedPlace = True
 				address = None
+				branchOffset = None
 				self.instructionBuffer.issue()
 				entryNumber = self.rob.add(instruction[0],instruction[1],None)
 
-				if s == "MUL" or s == "ADD" or s == "LDST" or s == "Logic":
-
-					if self.registerStatus.registers[instruction[2]] == None :
-						readySource1 = instruction[2]
-						notReadySource1 = None
-					else :
-						readySource1 = None
-						notReadySource1 = instruction[2]
-
-					if 'r' in instruction[3] and self.registerStatus.registers[instruction[3]] == None:
-						readySource2 = instruction[3]
-						notReadySource2 = None
-					elif 'r' in instruction[3] and self.registerStatus.registers[instruction[3]] != None:
-						readySource2 = None
-						notReadySource2 = instruction[3]
+				if s == "MUL" or s == "ADD" or s == "LDST" or s == "LOGIC":
+					if instruction[0] == 'beq':
+						# print(instruction)
+						if self.registerStatus.registers[instruction[2]] == None:
+							readySource2 = instruction[2]
+							notReadySource2 = None
+						else:
+							readySource2 = None
+							notReadySource2 = instruction[2]
+						if self.registerStatus.registers[instruction[1]] == None :
+							readySource1 = instruction[1]
+							notReadySource1 = None
+						else :
+							readySource1 = None
+							notReadySource1 = instruction[1]
+						branchOffset = instruction[4]
+						address = instruction[3]
+						# print(address, branchOffset, "First")
 					else:
-						readySource2 = instruction[3]
-						notReadySource2 = None
-				else:
-					pass
+						if self.registerStatus.registers[instruction[2]] == None :
+							readySource1 = instruction[2]
+							notReadySource1 = None
+						else :
+							readySource1 = None
+							notReadySource1 = instruction[2]
+
+						if 'r' in instruction[3] and self.registerStatus.registers[instruction[3]] == None:
+							readySource2 = instruction[3]
+							notReadySource2 = None
+						elif 'r' in instruction[3] and self.registerStatus.registers[instruction[3]] != None:
+							readySource2 = None
+							notReadySource2 = instruction[3]
+						else:
+							readySource2 = instruction[3]
+							notReadySource2 = None
 				self.registerStatus.registers[instruction[1]] = entryNumber
-				self.reservationStations[i].reserve(instruction[0], readySource1 , readySource2 , notReadySource1 , notReadySource2 , entryNumber , address)
+				# print(address, branchOffset, "SECOND")
+				self.reservationStations[i].reserve(instruction[0], readySource1 , readySource2 , notReadySource1 , notReadySource2 , entryNumber , address, branchOffset)
+				# print(self.reservationStations[i].address, self.reservationStations[i].branchOffset)
 				break
 		if reservedPlace:
 			return True
@@ -152,9 +181,9 @@ class Tomasulo(object):
 			# print("kkkkk")
 			# print(currentStation.currentCycles )
 			if currentStation.busy and currentStation.currentCycles == 0:
-				result = self.calculate(currentStation.op , currentStation.readySource1, currentStation.readySource2)
+				result = self.calculate(currentStation.op , currentStation.readySource1, currentStation.readySource2, currentStation.address, currentStation.branchOffset)
 				# print(result, "!!@#!@#!$")
-				if self.rob.ROB_Entries[currentStation.dest % self.rob.size] != None
+				if self.rob.ROB_Entries[currentStation.dest % self.rob.size] != None:
 					self.rob.update(result,currentStation.dest)
 					registerName = self.rob.ROB_Entries[currentStation.dest % self.rob.size].dest
 					# print(registerName)
@@ -167,13 +196,26 @@ class Tomasulo(object):
 	def commit(self):
 		# print(self.rob.ROB_Entries[self.rob.head % self.rob.size])
 		if self.rob.ROB_Entries[self.rob.head % self.rob.size] != None and self.rob.ROB_Entries[self.rob.head % self.rob.size].ready:
-			registerName = self.rob.ROB_Entries[self.rob.head % self.rob.size].dest
-			# print(registerName,  self.rob.ROB_Entries[self.rob.head % self.rob.size].value, "!!!!!!!!!!!!!!!!!!!!")
-			self.registerFile[registerName] = self.rob.ROB_Entries[self.rob.head % self.rob.size].value
-			self.rob.commit()
+			if self.rob.ROB_Entries[self.rob.head % self.rob.size].type == 'beq':
+				if self.toggleBranch == True:
+					# print("here")
+					self.toggleBranch = False
+					self.branchOverride = self.rob.ROB_Entries[self.rob.head % self.rob.size].value
+					self.rob.flush()
+					for i in range(len(self.reservationStations)):
+						self.reservationStations[i].flush()
+				else:
+					self.rob.commit()
+			elif self.rob.ROB_Entries[self.rob.head % self.rob.size].rtype == 'jmp':
+				self.rob.commit()
+			else:
+				registerName = self.rob.ROB_Entries[self.rob.head % self.rob.size].dest
+				# print(registerName,  self.rob.ROB_Entries[self.rob.head % self.rob.size].value, "!!!!!!!!!!!!!!!!!!!!")
+				self.registerFile[registerName] = self.rob.ROB_Entries[self.rob.head % self.rob.size].value
+				self.rob.commit()
 
 
-	def calculate(self,op, source1 , source2):
+	def calculate(self,op, source1 , source2, address, branchOffset):
 		if op == "addi":
 			a= self.registerFile[source1]
 			return a + int(source2)  # not a source its just a number
@@ -205,10 +247,23 @@ class Tomasulo(object):
 			a = self.registerFile[source1]
 			b = self.registerFile[source2]
 			return (~(a & b))
+		elif op == 'beq':
+			a = self.registerFile[source1]
+			b = self.registerFile[source2]
+			# print(a , b , branchOffset, address)
+			if a == b and int(branchOffset) > 0:
+				self.toggleBranch = True
+				return int(address) + int(branchOffset)
+			elif a != b and int(branchOffset) < 0:
+				self.toggleBranch = True
+				return int(address)
+			else:
+				return None 
 		else:
 			pass
 
-
+# def offset_conversion(number):
+# 	pass
 
 t = Tomasulo("file.txt")
 # t.commit()
